2418a8791fe119a845c452f96885196d917bf470
HandsomeGeorge <kawaiiiineko@gmail.com>
2022-05-07 18:17:13 +0300

    clean up

--- src/main/java/pantheonorbitalworks/hullmods/Torgue/Advanced/AdvancedConstants.java
+++ src/main/java/pantheonorbitalworks/hullmods/Torgue/Advanced/AdvancedConstants.java
@@ -1,16 +1,16 @@
 package pantheonorbitalworks.hullmods.Torgue.Advanced;
 
 public class AdvancedConstants {
-	public static float Hull = 15;
-	public static float Armor = 20;
+	public static float Hull = 25;
+	public static float Armor = 25;
 	public static int MaxSpeed = 15;
 	public static int Acceleration = 15;
 	public static int Deceleration = 15;
 	public static int TurnRate = 15;
 	public static int TurnAcceleration = 15;
-	public static int MinCrew = 20;
-	public static int PeakCR = 30;
-	public static int Supplies = 40;
+	public static int MinCrew = 25;
+	public static int PeakCR = 15;
+	public static int Supplies = 30;
 	public static float ShipSpeed = 10;
 	public static float MissileSpeed = 60;
 	public static int MissileTurn = 30;
--- src/main/java/pantheonorbitalworks/hullmods/Torgue/Expert/ExpertConstants.java
+++ src/main/java/pantheonorbitalworks/hullmods/Torgue/Expert/ExpertConstants.java
@@ -1,16 +1,16 @@
 package pantheonorbitalworks.hullmods.Torgue.Expert;
 
 public class ExpertConstants {
-	public static float Hull = 20;
+	public static float Hull = 30;
 	public static float Armor = 30;
 	public static int MaxSpeed = 20;
 	public static int Acceleration = 20;
 	public static int Deceleration = 20;
 	public static int TurnRate = 20;
 	public static int TurnAcceleration = 20;
-	public static int MinCrew = 25;
-	public static int PeakCR = 30;
-	public static int Supplies = 70;
+	public static int MinCrew = 30;
+	public static int PeakCR = 25;
+	public static int Supplies = 40;
 	public static float ShipSpeed = 15;
 	public static float MissileSpeed = 100;
 	public static int MissileTurn = 50;
